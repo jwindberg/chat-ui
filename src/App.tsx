807af7fe73d4c2238d0ceaefa88108ecaad79a31
jwindberg <john@windberg.name>
2024-01-16 18:47:41 -0800

    initial check in

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import Chat from "./chat/Chat";
+
+const App = () => (
+  <Chat />
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/atoms/store.ts
@@ -0,0 +1,6 @@
+import {atom} from "jotai/vanilla";
+
+export const messagesAtom = atom([
+    {user:"ChatGpt",message:"Hello, I'm ChatGpt!", messageType: "RECEIVED"},
+    {user:"Me",message:"Hello, I'm Me!", messageType: "SENT"}
+]);
